63bb1a5f4942a3d9cdc55aa72c8ee490ea82bcac
Richard Koene <rkoene@gmail.com>
2020-09-12 14:55:03 +0200

    Vereenvoudiging

--- exchange_pool.py
+++ exchange_pool.py
@@ -5,41 +5,40 @@ def memberStatus():
     # Init status dicts
     odcStatus = {"Aan":0, "Uit":0, "Up":0, "Down":0}
     apdStatus = {"Aan":0, "Uit":0, "Up":0, "Down":0}
+    memberStatus = {"O":odcStatus,"A":apdStatus}
 
     # Uitvragen huidige status van de poolmembers
     poolMembers = msF5.get_poolmembers("dev-adc01.koene.tld",
                                        "SSCICT~exch_test",
                                        "exch_test_pool")
     for member in poolMembers['items']:
-        if str(member['address']).startswith(dictSubnets["O"]):
-            if member['session'] == "user-disabled":
-                odcStatus["Uit"] += 1
-            else:
-                odcStatus["Aan"] += 1
-            if member['state'] == "down":
-                odcStatus["Down"] += 1
-            else:
-                odcStatus["Up"] += 1
-        elif str(member['address']).startswith(dictSubnets["A"]):
-            if member['session'] == "user-disabled":
-                apdStatus["Uit"] += 1
-            else:
-                apdStatus["Aan"] += 1
-            if member['state'] == "down":
-                apdStatus["Down"] += 1
-            else:
-                apdStatus["Up"] += 1
+        dcMember = ""
+        if str(member['address']).startswith(dictSubnets["O"]): dcMember = "O"
+        if str(member['address']).startswith(dictSubnets["A"]): dcMember = "A"
+        
+        if member['session'] == "user-disabled":
+            memberStatus[dcMember]["Uit"] += 1
+        else:
+            memberStatus[dcMember]["Aan"] += 1
+        if member['state'] == "down":
+            memberStatus[dcMember]["Down"] += 1
+        else:
+            memberStatus[dcMember]["Up"] += 1
 
     print ("Huidige status poolmembers:")
-    print ("+--------------------------------------------+")
-    print ("| ODC                                        |")
-    print ("+-----------+----------+----------+----------+")
-    print (str("|  Aan : {:2d} | Uit : {:2d} | Up : {:2d} | Down : {:2d} |").format(odcStatus["Aan"],odcStatus["Uit"],odcStatus["Up"],odcStatus["Down"]))
-    print ("+-----------+----------+----------+----------+")
-    print ("| APD                                        |")
-    print ("+-----------+----------+----------+----------+")
-    print (str("|  Aan : {:2d} | Uit : {:2d} | Up : {:2d} | Down : {:2d} |").format(apdStatus["Aan"],apdStatus["Uit"],apdStatus["Up"],apdStatus["Down"]))
-    print ("+-----------+----------+----------+----------+")
+    print ("+-----+-----------+----------+---------+-----------+")
+    print (str("| ODC |  Aan : {:2d} | Uit : {:2d} | Up : {:2d} | Down : {:2d} |")\
+        .format(memberStatus["O"]["Aan"],
+                memberStatus["O"]["Uit"],
+                memberStatus["O"]["Up"],
+                memberStatus["O"]["Down"]))
+    print ("+-----+-----------+----------+---------+-----------+")
+    print (str("| APD |  Aan : {:2d} | Uit : {:2d} | Up : {:2d} | Down : {:2d} |")\
+        .format(memberStatus["A"]["Aan"],
+                memberStatus["A"]["Uit"],
+                memberStatus["A"]["Up"],
+                memberStatus["A"]["Down"]))
+    print ("+-----+-----------+----------+---------+-----------+")
 
 # Aanmaken F5 microservice object
 msF5 = F5_ms.F5_microservice()
@@ -50,8 +49,11 @@ dictSubnets = {"O":"172.17.1.", "A":"172.18.2."}
 # Tonen doel van het script
 print ("\n")
 print ("\n")
-print ("Met deze tool kan je in 1 keer alle Exchange poolmembers in Rijswijk of Apeldoorn tegelijk enablen of disablen.")
-print ("Controleer vooraf de huidige status. Deze tool doet dat niet. Het is dus mogelijk om hiermee alle exchange servers te disablen.")
+print ("Met deze tool kan je in 1 keer alle Exchange poolmembers in " + \
+       "Rijswijk of Apeldoorn tegelijk enablen of disablen.")
+print ("Controleer vooraf de huidige status, vooral de nodes. " + \
+       "Deze tool doet dat niet. Het is dus mogelijk om hiermee " + \
+       "alle exchange servers te disablen.")
 print ("Menu keuze Q stopt het script.")
 print ("\n")
 
